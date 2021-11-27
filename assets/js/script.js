3735d53faeb2be449a6b8608cb3aeb6db86a301d
Michael Bashford <mikebashford@gmail.com>
2021-11-27 13:00:06 -0600

    Added temporary landing page to get started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,33 @@
+var questions = [];
+var answers = [];
+var timer;
+var score;
+var userInitials;
+
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
+function startGame()
+{
+
+}
 // THEN a timer starts and I am presented with a question
 // WHEN I answer a question
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
+function answerQuestion()
+{
+
+}
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
+function endGame()
+{
+
+}
 // WHEN the game is over
 // THEN I can save my initials and score
+function saveHighScores()
+{
+
+}

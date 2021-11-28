6cb89577d0c1734045967b20f66ceb5c5ae7cc82
Michael Bashford <mikebashford@gmail.com>
2021-11-28 15:23:04 -0600

    Added landing pages for game and highscores, changed to array of objects to iterate through instead of multiple arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,43 @@
-var questions = ["Inside which HTML element do we put the JavaScript?", "Where is the correct place to insert a JavaScript?", "What is the correct syntax for referring to an external script called 'xxx.js'?", "How do you write 'Hello World' in an alert box?", "How do you create a function in JavaScript?", "How do you call a function named 'myFunction'?", "How to write an IF statement in JavaScript?", "How does a WHILE loop start?", "How does a FOR loop start?", "How can you add a comment in a JavaScript?", "What is the correct way to write a JavaScript array?" ];
-var correctAnswers = ["<script>", "Both the <head> section and the <body> section are correct", "<script src='xxx.js'>", "alert('Hello World');", "function myFunction()", "myFunction()", "if (i == 5)", "while (i <= 10)", "for (i = 0; i <= 5; i++)", "//This is a comment", "var colors = ['red', 'green', 'blue']"];
-var timerText = document.querySelector(".timer-text");
-var timeLeft = document.querySelector(".timer-num");
-var userInitials = document.querySelector(".enter-initials");
-var startGameText = document.querySelector("#start-game");
-var startButton = document.querySelector("#start");
-var saveScore = document.querySelector("#save");
-var questionText = document.querySelector("#question");
-var choiceButtons = document.querySelector("#choices");
-const questionOne = ["<script>", "<js>", "<javascript>", "<scripting>"]
-const questionTwo = ["The <body> section", "Both the <head> section and the <body> section are correct", "The <head> section", "The <footer> section"];
-const questionThree = ["<script src='xxx.js'", "<script href='xxx.js'>", "<script name='xxx.js'>", "<script class='xxx.js'>"];
-const questionFour = [ "alert('Hello World');", "msgBox('Hello World');", "msg('Hello World');", "alertBox('Hello World');"];
-const questionFive = ["function myFunction()", "function = myFunction()", "function:myFunction()", "function = new function()"];
-const questionSix = ["myFunction()", "call myFunction()", "call function myFunction()", "myFunction.call()"];
-const questionSeven = ["if (i == 5)", "if i == 5 then", "if i = 5 then", "if i = 5"];
-const questionEight = ["while (i <= 10)", "while i = 1 to 10", "while (i <= 10; i++)", "while(i = 10"];
-const questionNine = ["for (i = 0; i <= 5; i++)", "for (i = 0; i <= 5)", "for i = 1 to 5", "for (i <= 5; i++)"];
-const questionTen = ["//This is a comment", "'This is a comment", "<!--This is a comment-->", "!-This is a comment-!"];
-var questionChoices = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen];
-var score;
+let questions = [
+  {question: "Inside which HTML element do we put the JavaScript?", choice1: "<script>" , choice2: "<js>", choice3: "<javascript>", choice4: "<scripting>", correctAnswer: 1}, 
+  {question:"Where is the correct place to insert a JavaScript?" , choice1: "The <body> section", choice2: "Both the <head> section and the <body> section are correct", choice3: "The <head> section", choice4: "The <footer> section", correctAnswer: 2}, 
+  {question:"What is the correct syntax for referring to an external script called 'xxx.js'?", choice1: "<script src='xxx.js'" , choice2: "<script href='xxx.js'>", choice3: "<script name='xxx.js'>", choice4: "<script class='xxx.js'>", correctAnswer: 1}, 
+  {question:"How do you write 'Hello World' in an alert box?", choice1: "alert('Hello World');", choice2: "msgBox('Hello World');", choice3: "msg('Hello World');", choice4: "alertBox('Hello World');", correctAnswer: 1},
+  {question:"How do you create a function in JavaScript?", choice1: "function myFunction()", choice2: "function = myFunction()", choice3: "function:myFunction()", choice4: "function = new function()", correctAnswer: 1},
+  {question:"How do you call a function named 'myFunction'?", choice1: "myFunction()" , choice2: "call myFunction()", choice3: "call function myFunction()", choice4: "<scripting>", correctAnswer: 1},
+  {question:"How to write an IF statement in JavaScript?", choice1: "if (i == 5)" , choice2: "if i == 5 then", choice3: "if i = 5 then", choice4: "if i = 5", correctAnswer: 1},
+  {question:"How does a WHILE loop start?", choice1: "while (i <= 10)" , choice2: "while i = 1 to 10", choice3: "while (i <= 10; i++)", choice4: "while(i = 10", correctAnswer: 1},
+  {question:"How does a FOR loop start?", choice1: "for (i = 0; i <= 5; i++)" , choice2: "for (i = 0; i <= 5)", choice3: "for i = 1 to 5", choice4: "for (i <= 5; i++)", correctAnswer: 1},
+  {question:"How can you add a comment in a JavaScript?", choice1: "//This is a comment", choice2: "'This is a comment", choice3: "<!--This is a comment-->", choice4: "!-This is a comment-!", correctAnswer: 1}];
+  
+const timerText = document.querySelector(".timer-text");
+const timeLeft = document.querySelector(".timer-num");
+const question = document.querySelector("#question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const saveScore = document.querySelector("#save");
+
+let currentQuestion = {};
+let score = 0;
+let questionCounter = 0;
+let acceptAnswer = true;
+let availableQuestions = [];
 
 // GIVEN I am taking a code quiz
 // WHEN I click the start button
 function startGame()
 {
-  startGameText.setAttribute("style", "display:none;");
+  score = 0;
+  questionCounter = 0;
+  availableQuestions = [...questions];
   startTimer();
+  getQuestion();
 }
 
 // THEN a timer starts and I am presented with a question
 function startTimer()
 {
-  giveQuestion();
   timer = 60;
-  timeLeft.textContent = timer;
+  timeLeft.innerText = timer;
   setInterval(function()
   {
     if(timer > 0)
@@ -53,41 +56,38 @@ function startTimer()
 // THEN I am presented with another question
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
-function giveQuestion()
+function getQuestion()
 {
-
-  for(i = 0; i < questions.length; i++)
+  if(availableQuestions.length === 0)
   {
-    var h2 = document.createElement('h2');
-    h2.innerHTML += questions[i];
-    questionText.textContent = h2.innerHTML;
-    questionText.setAttribute("style", "text-align:center; font-size:24px;");
-    for(i = 0; i < 4; i++)
-    {
-      var button = document.createElement('button');
-      $(button).addClass("btn btn-primary d-flex m-2 col-3");
-      button.textContent = questionOne[i];
-      document.getElementById("choices").appendChild(button);
-      document.getElementById("choices").setAttribute("style", "display: block;");
-    }
-    if(true)
-    {
-      console.log("Correct!");
-      return;
-    }
-    else
-    {
-      console.log("Wrong!")
-      return;
-    }
+    endGame();
+    return;
   }
+  currentQuestion = availableQuestions[questionCounter];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice =>
+  {
+    const number = choice.dataset['value'];
+    choice.innerText = currentQuestion[number];
+  });
+
+  // choices.forEach((choice) =>{
+  //   choice.addEventListener("click", event =>{
+  //     selectAnswer = e.target;
+  //   })
+  //   var selectedAnswer = selectAnswer.dataset["answer"];
+  //   console.log(selectedAnswer);
+  // })
+  //questionCounter++;
+  // getQuestion();
 }
+
 
 // WHEN all questions are answered or the timer reaches 0
 // THEN the game is over
 function endGame()
 {
-  userInitials.setAttribute("style", "display:block;");
   console.log("Game is over, enter initials: ");
   return;
 }
@@ -96,7 +96,7 @@ function endGame()
 function saveHighScores()
 {
   console.log("High score saved!");
+  return;
 }
 
-startButton.addEventListener("click", startGame);
-saveScore.addEventListener("click", saveHighScores);
+startGame();

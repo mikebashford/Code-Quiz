d5378ec140fe64c14c430543297f619d9a40fb6a
Michael Bashford <mikebashford@gmail.com>
2021-11-28 20:24:25 -0600

    Styled buttons and page to be more consistent, mixed up answers so they aren't all the first option

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 var questions = [
   {question: "Inside which HTML element do we put the JavaScript?", choice1: "<script>" , choice2: "<js>", choice3: "<javascript>", choice4: "<scripting>", correctAnswer: 1}, 
   {question:"Where is the correct place to insert a JavaScript?" , choice1: "The <body> section", choice2: "Both the <head> section and the <body> section are correct", choice3: "The <head> section", choice4: "The <footer> section", correctAnswer: 2}, 
-  {question:"What is the correct syntax for referring to an external script called 'xxx.js'?", choice1: "<script src='xxx.js'" , choice2: "<script href='xxx.js'>", choice3: "<script name='xxx.js'>", choice4: "<script class='xxx.js'>", correctAnswer: 1}, 
-  {question:"How do you write 'Hello World' in an alert box?", choice1: "alert('Hello World');", choice2: "msgBox('Hello World');", choice3: "msg('Hello World');", choice4: "alertBox('Hello World');", correctAnswer: 1},
+  {question:"What is the correct syntax for referring to an external script called 'xxx.js'?", choice1: "<script class='xxx.js'" , choice2: "<script href='xxx.js'>", choice3: "<script name='xxx.js'>", choice4: "<script src='xxx.js'>", correctAnswer: 4}, 
+  {question:"How do you write 'Hello World' in an alert box?", choice1: "msgBox('Hello World');", choice2: "alert('Hello World');", choice3: "msg('Hello World');", choice4: "alertBox('Hello World');", correctAnswer: 2},
   {question:"How do you create a function in JavaScript?", choice1: "function myFunction()", choice2: "function = myFunction()", choice3: "function:myFunction()", choice4: "function = new function()", correctAnswer: 1},
-  {question:"How do you call a function named 'myFunction'?", choice1: "myFunction()" , choice2: "call myFunction()", choice3: "call function myFunction()", choice4: "<scripting>", correctAnswer: 1},
-  {question:"How to write an IF statement in JavaScript?", choice1: "if (i == 5)" , choice2: "if i == 5 then", choice3: "if i = 5 then", choice4: "if i = 5", correctAnswer: 1},
+  {question:"How do you call a function named 'myFunction'?", choice1: "call myFunction()" , choice2: "myFunction()", choice3: "call function myFunction()", choice4: "<scripting>", correctAnswer: 2},
+  {question:"How to write an IF statement in JavaScript?", choice1: "if i = 5" , choice2: "if i == 5 then", choice3: "if i = 5 then", choice4: "if (i == 5)", correctAnswer: 4},
   {question:"How does a WHILE loop start?", choice1: "while (i <= 10)" , choice2: "while i = 1 to 10", choice3: "while (i <= 10; i++)", choice4: "while(i = 10", correctAnswer: 1},
-  {question:"How does a FOR loop start?", choice1: "for (i = 0; i <= 5; i++)" , choice2: "for (i = 0; i <= 5)", choice3: "for i = 1 to 5", choice4: "for (i <= 5; i++)", correctAnswer: 1},
-  {question:"How can you add a comment in a JavaScript?", choice1: "//This is a comment", choice2: "'This is a comment", choice3: "<!--This is a comment-->", choice4: "!-This is a comment-!", correctAnswer: 1}];
+  {question:"How does a FOR loop start?", choice1: "for (i = 0; i <= 5)" , choice2: "for (i = 0; i <= 5; i++)", choice3: "for i = 1 to 5", choice4: "for (i <= 5; i++)", correctAnswer: 2},
+  {question:"How can you add a comment in a JavaScript?", choice1: "<!--This is a comment-->", choice2: "'This is a comment", choice3: "//This is a comment", choice4: "!-This is a comment-!", correctAnswer: 3}];
   
 var timerText = document.querySelector(".timer-text");
 var timeLeft = document.querySelector(".timer-num");
